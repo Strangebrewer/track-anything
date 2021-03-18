88d5e838f6b79c0c4e13b301fb924714c6ebada8
Strangebrewer <BKAShambala@gmail.com>
2021-03-18 17:06:42 -0600

    add typescript and builder server

---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3001;
+import { json, text, urlencoded } from 'body-parser';
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(text());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+});
+
+app.listen(PORT, () => {
+    console.log(`API Server now listening on PORT ${PORT}`);
+});
