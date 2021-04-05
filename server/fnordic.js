3e91237e38e269c33bd1c28e8aa77d28b6b0ab83
Keith Allmon <keith.allmon@ifit.com>
2021-04-05 17:46:56 -0600

    fiddling

---
+++ server/fnordic.js
@@ -0,0 +1,7 @@
+var isEqual = require('date-fns/isEqual');
+var format = require('date-fns/format');
+var startOfToday = require('date-fns/startOfToday');
+var startOfTomorrow = require('date-fns/startOfTomorrow');
+
+console.log('startOfToday():::', startOfToday());
+console.log('startOfTomorrow():::', startOfTomorrow());
