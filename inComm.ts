add1f50c3d9d80f5ba142f269d15901ad056f34a
Keith Allmon <keith.allmon@ifit.com>
2021-04-16 17:07:14 -0600

    more InComm testing and dummy code

--- inComm.ts
+++ inComm.ts
@@ -1,57 +1,67 @@
 import axios from "axios";
 
 export default class InCommApiClient {
+  private clientId: string;
+  private clientSecret: string;
   private authToken: string;
 
-  constructor(authToken: string = null) {
-    this.authToken = authToken;
+  constructor(clientId: string, clientSecret: string) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.authToken = null;
   }
 
-  private async authorize(): Promise<string> {
-    return "auth-token";
+  private async authorize(): Promise<void> {
+    this.authToken = "fake-auth-token";
   }
 
   private async cancel(transactionId: string): Promise<string> {
-    if (!this.authToken) {
-      this.authorize();
-    }
     return transactionId;
   }
 
-  public async getStatus(pin: string): Promise<string> {
-    if (!this.authToken) {
-      this.authorize();
-    }
-    const getStatusResponse = await axios.post(`${process.env}/rtg-tv/v2/api/getStatus`, {
-      headers: {
-        Authentication: `Bearer ${this.authToken}`,
-        "Content-Type": "application/json",
-      },
-      data: {
-        client_id: process.env.INCOMM_CLIENT_ID,
-        RetailTransactionTVRequest: {
-          dateTime: new Date().toISOString(),
-          product: {
-            partnerName: "ifit",
-            productCat: "PIN",
-            inventoryID: pin,
-          },
-          source: "Web?",
-          transactionID: "?",
-        },
-      },
-    });
-
-    const responseText = getStatusResponse.data?.RetailTransactionTVResponse?.responseText;
-
-    if (!responseText) {
-      throw Error("Bad pin");
-    }
-
-    return responseText;
+  public async getStatus(code: string): Promise<string> {
+    return code;
   }
 
   public async redeem(code: string): Promise<string> {
-    return code;
+    if (this.authToken) {
+      this.authorize();
+    }
+
+    const transactionId = "123456789";
+
+    try {
+      const redeemResponse = await axios.post(`${process.env}/rtg-tv/v2/redeem`, {
+        headers: {
+          Authentication: `Bearer ${this.authToken}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          RetailTransactionTVRequest: {
+            dateTime: new Date().toISOString(),
+            product: {
+              partnerName: "ifit",
+              productCat: "PIN",
+              inventoryId: code,
+            },
+            source: "Web",
+            transactionId,
+          },
+        },
+      });
+
+      const responseCode = redeemResponse.data?.RetailTransactionTVResponse?.responseCode;
+
+      if (responseCode && responseCode !== 0 && responseCode !== 136) {
+        const responseText = redeemResponse.data?.RetailTransactionTVResponse?.responseText;
+        throw Error(responseText);
+      }
+
+      // leaving this return value until we have more info on what should be returned and can build an accurate interface
+      return code;
+    } catch (err) {
+      // timeout and cancel logic will be added in another PR
+      throw Error(err.message);
+    }
   }
 }
